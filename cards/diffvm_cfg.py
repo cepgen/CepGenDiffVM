f514294265c7394be2532d743c871c6816b65b7b
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-10-04 16:17:57 +0200

    DiffVM: improved documentation and PDG definition

--- cards/diffvm_cfg.py
+++ cards/diffvm_cfg.py
@@ -1,9 +1,19 @@
 import Config.Core as cepgen
 #from Config.pythia8_cff import pythia8 as hadroniser
+from Config.PDG_cfi import PDG
 from Config.logger_cfi import logger
 from Config.generator_cfi import generator as _gen
 from Integrators.vegas_cfi import vegas as integrator
 
+
+DiffVMPDG = cepgen.Parameters(
+    Jpsi      = Parameters(pdgid = 443),
+    Upsilon1S = Parameters(pdgid = 553),
+    proton    = Parameters(pdgid = 2212),
+    neutron   = Parameters(pdgid = 2112),
+    Upsilon2S = Parameters(pdgid = 100553),
+    Upsilon3S = Parameters(pdgid = 200553),
+)
 
 logger.enabledModules += (
 #    'DiffVM.*',
@@ -14,11 +24,11 @@ import Config.DiffVM_cff as diffvm
 
 process = cepgen.Module('diffvm',
     processParameters = diffvm.defaultProcessParameters.clone(
-        #vmFlavour = 23, # Z
+        vmFlavour = DiffVMPDG.Upsilon1S,
         #protonMode = diffvm.BeamMode.StandardFragmentation,
     ),
     inKinematics = cepgen.Parameters(
-        pdgIds = (11, 2212),
+        pdgIds = (PDG.electron, PDG.proton),
         pz = (27.55, 820.),
     ),
     outKinematics = cepgen.Parameters(
