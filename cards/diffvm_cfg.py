forthommel/cepgen

DiffVM: more filling

Cards/diffvm_cfg.py
@@ -1,0 +1,31 @@
+import Config.Core as cepgen
+from Config.integrators_cff import vegas as integrator
+#from Config.pythia8_cff import pythia8 as hadroniser
+from Config.logger_cfi import logger
+
+logger.enabledModules += (
+    'DiffVM',
+)
+
+process = cepgen.Module('diffvm',
+    mode = cepgen.ProcessMode.ElasticElastic,
+    inKinematics = cepgen.Parameters(
+        pz = (6500., 6500.),
+        structureFunctions = cepgen.StructureFunctions.LUXlike,
+    ),
+    outKinematics = cepgen.Parameters(
+        pair = 13,
+        pt = (25.,),
+        energy = (0.,),
+        eta = (-2.5, 2.5),
+        mx = (1.07, 1000.),
+    ),
+)
+
+#--- let the user specify the run conditions
+from Config.generator_cff import generator
+generator = generator.clone(
+    numEvents = 100000,
+    printEvery = 10000,
+)
+

cepgen/CepGenDiffVM

cards/diffvm_cfg.py
@@ -1,0 +1,31 @@
+import Config.Core as cepgen
+#from Config.pythia8_cff import pythia8 as hadroniser
+from Config.logger_cfi import logger
+from Config.generator_cfi import generator as _gen
+from Integrators.vegas_cfi import vegas as integrator
+
+
+logger.enabledModules += (
+    'DiffVM',
+)
+
+process = cepgen.Module('diffvm',
+    mode = cepgen.ProcessMode.ElasticElastic,
+    inKinematics = cepgen.Parameters(
+        pz = (6500., 6500.),
+        structureFunctions = cepgen.StructureFunctions.LUXlike,
+    ),
+    outKinematics = cepgen.Parameters(
+        pair = 13,
+        pt = (25.,),
+        energy = (0.,),
+        eta = (-2.5, 2.5),
+        mx = (1.07, 1000.),
+    ),
+)
+
+generator = _gen.clone(
+    numEvents = 100000,
+    printEvery = 10000,
+)
+