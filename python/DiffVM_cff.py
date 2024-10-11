forthommel/cepgen

DiffVM: adapted to new process API

python/Config/DiffVM_cff.py
@@ -1,6 +1,6 @@
-from containers_cfi import Module, Parameters
-from ProcessMode_cfi import ProcessMode
-from PDG_cfi import PDG
+from .containers_cfi import Module, Parameters
+from .ProcessMode_cfi import ProcessMode
+from .PDG_cfi import PDG
 
 class BeamMode:
     '''Beam particles treatment mode'''

python/Config/EPA_cfi.py
@@ -1,4 +1,4 @@
-from containers_cfi import Parameters
+from .containers_cfi import Parameters
 
 class EPAMode:
     WWA        = 1

cepgen/CepGenDiffVM

python/DiffVM_cff.py
@@ -1,6 +1,7 @@
-from containers_cfi import Module, Parameters
-from ProcessMode_cfi import ProcessMode
-from PDG_cfi import PDG
+from .containers_cff import Module, Parameters
+from .ProcessMode_cff import ProcessMode
+from .PDG_cfi import PDG
+
 
 class BeamMode:
     '''Beam particles treatment mode'''

python/EPA_cfi.py
@@ -1,4 +1,5 @@
-from containers_cfi import Parameters
+from .containers_cff import Parameters
+
 
 class EPAMode:
     WWA        = 1